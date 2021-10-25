116beb8915ab96a6c777e275bc7cd14d316e4136
Ayush Gangwani <gangwaniah@gmail.com>
2021-10-25 11:43:00 +0530

    Add solution for problem `D - RGB Triplets` Link: https://atcoder.jp/contests/abc162/tasks/abc162_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc162/submissions/26810780

---
+++ atcoder/abc162/D.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll                long long
+#define ld                long double
+#define get(type,args...) type args; read(args);
+#define putn(args...)     { put(args); cout<<"\n"; }
+#define rep(i,a,b)        for(int i=a; i<b; i++)
+#define repr(i,a,b)       for(int i=a; i>b; i--)
+#define vi                vector<long long>
+#define vs                vector<string>
+#define pq_max            priority_queue<long long>
+#define pq_min            priority_queue< long long , vi , greater<long long> >
+#define init(a,n)         vector<long long> a(n); for(int i=0; i<n; i++) { cin>>a[i]; }
+#define log(args...)      { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define logarr(arr,f,l)   { auto start=arr.begin(), end=arr.begin(); advance(start,f); advance(end,l); for(auto it=start; it!=end; it++) cout<<*it<<" "; cout<<"\n"; }
+#define debug(arr,f,l)    { int t=f; auto start=arr.begin(), end=arr.begin(); advance(start,f); advance(end,l); for(auto it=start; it!=end; it++) cerr<<t++<<"--> "<<*it<<"\n"; }
+#define all(x)            x.begin(), x.end()
+#define uniq(x)           x.erase(unique(all(x)),x.end());
+#define sz(x)             (int)x.size()
+#define mid(l,r)          (l+(r-l)/2)
+#define pii               pair<ll,ll>
+#define sumv(v,b,e)       accumulate(v.begin()+b,v.begin()+e,0LL)
+#define minv(v,b,e)       min_element(v.begin()+b,v.begin()+e)
+#define maxv(v,b,e)       max_element(v.begin()+b,v.begin()+e)
+#define pb                emplace_back
+#define pf                push_front
+#define fr                first
+#define sc                second
+#define bpc               __builtin_popcountll
+#define ctz               __builtin_ctzll
+#define clz               __builtin_clzll
+#define precise(n)        cout<<fixed<<setprecision(n)
+
+const int N = 100001, M = 998244353, mod = 1000000007;
+const ll MX = INT64_MAX, MN = INT64_MIN, oo = 1e18;
+
+//#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+//using namespace __gnu_pbds;
+using namespace std;
+
+vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
+
+template<typename T1, typename T2> istream& operator >> (istream& in, pair<T1,T2>& a) { in>>a.first>>a.second; return in; }
+template<typename T1, typename T2> ostream& operator << (ostream& out, pair<T1,T2> a) { out<<a.first<<" "<<a.second; return out; }
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) { cerr << *it << " = " << a << "\n"; err(++it, args...); }
+
+template<typename... T>
+void read(T&... args) { ((cin>>args), ...); }
+
+template<typename... T>
+void put(T&&... args) { ((cout<<args<<" "), ...);}
+
+void file_i_o()
+{
+    freopen("./tests/test01.txt", "r", stdin);
+    freopen("./tests/output01.txt", "w", stdout);
+}
+
+void solve(int T)
+{
+    get(int,n)
+    get(string,s)
+    s.insert(s.begin(),' ');
+    ll ans = 0;
+    vector<vector<int> > dp(3,vector<int> (n+1,0));
+    auto helper = [](char& c)
+    {
+        if(c == 'R') return 0;
+        else if(c == 'G') return 1;
+        else return 2;
+    };
+    rep(i,1,n+1)
+    {
+        rep(j,0,3) dp[j][i] = dp[j][i-1];
+        int ind = helper(s[i]);
+        dp[ind][i]++;
+    }
+    rep(i,2,n)
+    {
+        int ind = helper(s[i]);
+        if(ind == 0)
+        {
+            ans += dp[1][i-1]*(dp[2][n] - dp[2][i]);
+            ans += dp[2][i-1]*(dp[1][n] - dp[1][i]);
+        }
+        else if(ind == 1)
+        {
+            ans += dp[2][i-1]*(dp[0][n] - dp[0][i]);
+            ans += dp[0][i-1]*(dp[2][n] - dp[2][i]);
+        }
+        else 
+        {
+            ans += dp[0][i-1]*(dp[1][n] - dp[1][i]);
+            ans += dp[1][i-1]*(dp[0][n] - dp[0][i]);
+        }
+    }
+    rep(i,1,n+1)
+    {
+        rep(j,i+1,n+1)
+        {
+            if((i+j)%2) continue;
+            int mid = (i+j)/2;
+            if(s[i]!=s[j] and s[mid]!=s[j] and s[mid]!=s[i]) ans--;
+        }
+    }
+    putn(ans)
+}
+
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //file_i_o();
+
+    int t=1;
+    rep(i,1,t+1)
+    {
+        //cout<<"Case #"<<i<<": ";
+        solve(t);
+    }
+
+    #ifndef ONLINE_JUDGE
+    cerr<<"\ntime taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<"\n";
+    #endif
+    return 0;
+}
